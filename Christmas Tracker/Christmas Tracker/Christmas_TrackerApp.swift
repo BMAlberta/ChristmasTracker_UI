fb3649b5ba935b93fc0a2a752733661d89f1ee91
Brian <balbertax@gmail.com>
2021-01-03 22:53:37 -0500

    Initial project creation

---
+++ Christmas Tracker/Christmas Tracker/Christmas_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Christmas_TrackerApp.swift
+//  Christmas Tracker
+//
+//  Created by Brian Alberta on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Christmas_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Christmas Tracker/Christmas Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Christmas Tracker
+//
+//  Created by Brian Alberta on 1/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
